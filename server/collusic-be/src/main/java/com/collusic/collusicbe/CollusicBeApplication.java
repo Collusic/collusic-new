92585aabe04daa10d112d240f5f4fbff632796ac
sunghyuki <sunghyuk1609@gmail.com>
2021-12-27 15:36:37 +0900

    chore: initialize project

---
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/CollusicBeApplication.java
@@ -0,0 +1,13 @@
+package com.collusic.collusicbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollusicBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollusicBeApplication.class, args);
+    }
+
+}
---
+++ server/collusic-be/src/test/java/com/collusic/collusicbe/CollusicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collusic.collusicbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollusicBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
