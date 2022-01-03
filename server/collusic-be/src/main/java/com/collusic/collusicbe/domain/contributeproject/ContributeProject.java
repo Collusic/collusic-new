ec4a668cb4fed41c4c93b41921e9fdd6f85ee198
sunghyuki <sunghyuk1609@gmail.com>
2022-01-03 18:57:16 +0900

    feat: ContributeProject 엔티티 클래스 추가

--- server/collusic-be/src/main/java/com/collusic/collusicbe/domain/contributeproject/ContributeProject.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/domain/contributeproject/ContributeProject.java
@@ -1,10 +1,35 @@
 package com.collusic.collusicbe.domain.contributeproject;
 
 import com.collusic.collusicbe.domain.requestproject.RequestProject;
+import com.collusic.collusicbe.domain.upload.UploadFile;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class ContributeProject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CONTRIBUTE_PROJECT_ID")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "REQUEST_PROJECT_ID")
+    private RequestProject requestProjectId;
+
+    @Column(columnDefinition = "TEXT", length = 300, nullable = false)
+    private String content;
+
+    @Column(columnDefinition = "TEXT", length = 1500)
+    private String lyrics;
+
+    private Boolean adoptFlag;
+
+    @Embedded
+    private UploadFile uploadFile;
 
 }
