3673ce93c60a91036d293be94269a94f5b30a26e
sunghyuki <sunghyuk1609@gmail.com>
2022-01-07 21:29:29 +0900

    feat: RequestProeject 엔티티에 필요한 Enum 추가" - GenreEntity 엔티티 클래스 추가 - Gerne Enum 추가 - MoodEntity 엔티티 클래스 추가 - Mood Enum 추가

--- server/collusic-be/src/main/java/com/collusic/collusicbe/domain/genre/GenreEntity.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/domain/genre/GenreEntity.java
@@ -3,5 +3,6 @@ package com.collusic.collusicbe.domain.genre;
 import javax.persistence.Entity;
 
 @Entity
+@Table(name = "GENRE")
 public class GenreEntity {
 }
---
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/domain/mood/Mood.java
@@ -0,0 +1,20 @@
+package com.collusic.collusicbe.domain.mood;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public enum Mood {
+    UPLIFTING,
+    EPIC,
+    POWERFUL,
+    HAPPY,
+    HOPEFUL,
+    LOVE,
+    PLAYFUL,
+    REACEFUL,
+    SERIOUS,
+    DRAMATIC,
+    TENSE,
+    SAD,
+    DARK;
+}
