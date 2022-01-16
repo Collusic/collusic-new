2ac1841cba1990ba6b35f90555e2fcf3bd6ddbd5
spiderq2697 <spiderq10@gmail.com>
2022-01-16 17:04:24 +0900

    refactor : MainPage -> MainPageView 이름 및 경로 변경

--- client/src/view/MainPageView.tsx
+++ client/src/view/MainPageView.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { RequestListViewModel } from "../viewmodel/RequestListViewModel";
 import "./MainPage.scss";
 
-export const MainPage: React.FC = () => {
+export const MainPageView: React.FC = () => {
   return (
     <main className="mainpage">
       <RequestListViewModel />
