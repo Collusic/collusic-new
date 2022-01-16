3d5b0e539290aa2de4e71ad7c281fdcebfb4a471
sunghyuki <sunghyuk1609@gmail.com>
2022-01-17 00:40:17 +0900

    feat : 페이지네이션 기능 구현 Service 메서드 추가 - 페이지네이션 기능 비즈니스 로직 추가 - RequestProjectsWithPaginationDto 어노테이션 추가

--- server/collusic-be/src/main/java/com/collusic/collusicbe/service/RequestProjectService.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/service/RequestProjectService.java
@@ -1,12 +1,19 @@
 package com.collusic.collusicbe.service;
 
+import com.collusic.collusicbe.domain.requestproject.RequestProject;
 import com.collusic.collusicbe.domain.requestproject.RequestProjectRepository;
+import com.collusic.collusicbe.web.dto.RequestProjectResponseDto;
 import com.collusic.collusicbe.web.dto.RequestProjectSaveRequestDto;
+import com.collusic.collusicbe.web.dto.RequestProjectsWithPaginationDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +27,13 @@ public class RequestProjectService {
         String uploadFilePath = s3Service.upload(requestProjectSaveRequestDto.getMultipartFile(), "static");
         requestProjectSaveRequestDto.setUploadFilePath(uploadFilePath);
         return requestProjectRepository.save(requestProjectSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public RequestProjectsWithPaginationDto getRequestProjectsWithPagination(Pageable pageable) {
+        Page<RequestProject> all = requestProjectRepository.findAll(pageable);
+        List<RequestProjectResponseDto> requestProjectResponseDtos = all.getContent().stream().map(requestProject -> new RequestProjectResponseDto(requestProject)).collect(Collectors.toList());
+        int totalPages = all.getTotalPages();
+        return new RequestProjectsWithPaginationDto(requestProjectResponseDtos, totalPages);
     }
 }
--- server/collusic-be/src/main/java/com/collusic/collusicbe/web/dto/RequestProjectsWithPaginationDto.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/dto/RequestProjectsWithPaginationDto.java
@@ -1,7 +1,12 @@
 package com.collusic.collusicbe.web.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Getter
+@NoArgsConstructor
 public class RequestProjectsWithPaginationDto {
     private List<RequestProjectResponseDto> requestProjectResponseDtos;
     private int totalPages;
