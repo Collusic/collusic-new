67c93ad062408fa6df7c43e670c29f11d435fdca
sunghyuki <sunghyuk1609@gmail.com>
2022-01-19 23:36:03 +0900

    feat : 기여작 생성 API 컨트롤러 메서드 추가

---
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/ContributeProjectApiController.java
@@ -0,0 +1,23 @@
+package com.collusic.collusicbe.web.controller;
+
+import com.collusic.collusicbe.domain.contributeproject.ContributeProject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ContributeProjectApiController {
+
+    private final ContributeProjectService contributeProjectService;
+
+    @PostMapping("/api/contributeprojects")
+    public ResponseEntity<ContributeProjectResponseDto> save(@ModelAttribute ContributeProjectSaveRequestDto contributeProjectSaveRequestDto) {
+        ContributeProjectResponseDto contributeProjectResponseDto = contributeProjectService.save(contributeProjectSaveRequestDto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(contributeProjectResponseDto);
+    }
+}
