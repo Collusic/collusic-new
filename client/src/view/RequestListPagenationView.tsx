78fe8338626cc899f61eddff93649a3f79c439db
spiderq2697 <spiderq10@gmail.com>
2022-01-19 13:01:19 +0900

    fix : RequestListPagenationView 에러 해결 - pagenationList 가 존재할 때만 해당 배열에 map api 적용

--- client/src/view/RequestListPagenationView.tsx
+++ client/src/view/RequestListPagenationView.tsx
@@ -1,27 +1,22 @@
 import React from "react";
-import "./RequestListPagenation.scss";
 
 type RequestListPagenationProps = {
   pagenationList: number[];
-
-  onClickHandler(e: React.MouseEvent<HTMLButtonElement>): void;
+  // onClickHandler(e: React.MouseEvent<HTMLButtonElement>): void;
 };
 
 export const RequestListPagenationView: React.FC<
   RequestListPagenationProps
-> = ({ pagenationList, onClickHandler }) => {
+> = ({ pagenationList }) => {
   return (
     <article className="pagenation">
       <section className="left-arrow">{"<"}</section>
-      {pagenationList.map((pageNumber, index) => (
-        <section
-          className="page-number"
-          id={`${index}`}
-          onClick={onClickHandler}
-        >
-          {pageNumber}
-        </section>
-      ))}
+      {pagenationList &&
+        pagenationList.map((pageNumber, index) => (
+          <section className="page-number" id={`${index}`}>
+            {pageNumber}
+          </section>
+        ))}
       <section className="right-arrow">{">"}</section>
     </article>
   );
