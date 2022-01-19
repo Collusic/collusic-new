6c3310919699bbf585a70cd635ac5b34626419cf
gxxrxn <kkr1226@naver.com>
2022-01-20 03:45:58 +0900

    feat: 요청작 생성 페이지 뷰모델에 요청 내용 입력 컴포넌트 뷰 추가
    - CreateRequestViewModel에 CreateContentView 추가

--- client/src/viewmodel/CreateRequestViewModel.tsx
+++ client/src/viewmodel/CreateRequestViewModel.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { CreateTitleView } from "../view/CreateTitleView";
+import { CreateContentView } from "../view/CreateContentView";
 
 export const CreateRequestViewModel: React.FC = () => {
   return (
     <React.Fragment>
       <CreateTitleView />
+      <CreateContentView />
     </React.Fragment>
   );
 };
