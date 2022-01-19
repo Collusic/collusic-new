27c3d576eb6f889ed6d0df54b3bc11b01d1b2bfa
gxxrxn <kkr1226@naver.com>
2022-01-20 03:28:47 +0900

    feat: CreateRequestViewModel에 제목 입력 컴포넌트 뷰 추가
    - CreateRequestViewModel에 CreateTitleView 추가

---
+++ client/src/viewmodel/CreateRequestViewModel.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CreateTitleView } from "../view/CreateTitleView";
+
+export const CreateRequestViewModel: React.FC = () => {
+  return (
+    <React.Fragment>
+      <CreateTitleView />
+    </React.Fragment>
+  );
+};
