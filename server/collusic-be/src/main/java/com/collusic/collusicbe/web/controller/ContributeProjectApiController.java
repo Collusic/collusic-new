33ce9d03241c5352f3b4292b4f139853fa2b146b
sunghyuki <sunghyuk1609@gmail.com>
2022-01-20 00:42:24 +0900

    refactor : ContributeProjectApiController 기여작 생성 API 경로 수정

--- server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/ContributeProjectApiController.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/ContributeProjectApiController.java
@@ -1,9 +1,11 @@
 package com.collusic.collusicbe.web.controller;
 
+import com.collusic.collusicbe.web.dto.ContributeProjectSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,8 @@ public class ContributeProjectApiController {
 
     private final ContributeProjectService contributeProjectService;
 
-    @PostMapping("/api/contributeprojects")
-    public ResponseEntity<ContributeProjectResponseDto> save(@ModelAttribute ContributeProjectSaveRequestDto contributeProjectSaveRequestDto) {
+    @PostMapping("/api/requestprojects/{id}/contributeprojects)
+    public ResponseEntity<ContributeProjectResponseDto> save(@ModelAttribute ContributeProjectSaveRequestDto contributeProjectSaveRequestDto, @PathVariable Long id) {
         ContributeProjectResponseDto contributeProjectResponseDto = contributeProjectService.save(contributeProjectSaveRequestDto);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(contributeProjectResponseDto);
