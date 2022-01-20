6dfd3e58f22ee2754a1d7aabfbca3711cb8d5d72
Hongdonggeon <ghd700@naver.com>
2022-01-20 13:04:41 +0900

    feat : ContributeProjectApiController 기여작 수정 api 메서드 추가

--- server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/ContributeProjectApiController.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/ContributeProjectApiController.java
@@ -6,10 +6,7 @@ import com.collusic.collusicbe.web.dto.ContributeProjectSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -22,6 +19,13 @@ public class ContributeProjectApiController {
     @PostMapping("/api/requestprojects/{id}/contributeprojects")
     public ResponseEntity<ContributeProjectResponseDto> save(@ModelAttribute ContributeProjectSaveRequestDto contributeProjectSaveRequestDto, @PathVariable Long id) throws IOException {
         ContributeProjectResponseDto contributeProjectResponseDto = contributeProjectService.save(contributeProjectSaveRequestDto, id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(contributeProjectResponseDto);
+    }
+
+    @PutMapping("/api/requestprojects/{requestId}/contributeprojects/{contributeId}")
+    public ResponseEntity<ContributeProjectResponseDto> update(@ModelAttribute ContributeProjectUpdateRequestDto contributeProjectUpdateRequestDto, @PathVariable Long requestId, @PathVariable Long contributeId){
+        ContributeProjectResponseDto contributeProjectResponseDto = contributeProjectService.update(contributeProjectUpdateRequestDto, requestId, contributeId);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(contributeProjectResponseDto);
     }
