288d1f25a234eb38a7a70e7287737db9ee7a7384
sunghyuki <sunghyuk1609@gmail.com>
2022-01-23 17:39:49 +0900

    fix : 스프링 부트 테스트 어노테이션 제거

--- server/collusic-be/src/test/java/com/collusic/collusicbe/CollusicBeApplicationTests.java
+++ server/collusic-be/src/test/java/com/collusic/collusicbe/CollusicBeApplicationTests.java
@@ -1,13 +1,10 @@
 package com.collusic.collusicbe;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class CollusicBeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
