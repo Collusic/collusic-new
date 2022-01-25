c6ca0712966f8c3ca11b56b4badc314596bc2cd1
spiderq2697 <spiderq10@gmail.com>
2022-01-25 13:46:18 +0900

    feat : 특정 요청작에 대한 Field State 생성

--- client/src/model/detailRequestProjectModel.ts
+++ client/src/model/detailRequestProjectModel.ts
@@ -1,19 +1,63 @@
 import { atom, selector } from "recoil";
+import { ContributeProject } from "../types/contributeProjectType";
+import { RequestProjectType } from "../types/requestProjectType";
+import { User } from "../types/userType";
+// import { API } from "../utils/axios";
 
-import { API } from "../utils/axios";
-
-const detailRequestProjectId = atom({
-  key: "detailRequestProjectId",
+const detailRequestProjectIdState = atom({
+  key: "detailRequestProjectIdState",
   default: "0",
 });
 
-const getDetailRequest = selector({
-  key: "getDetailRequest",
-  get: async ({ get }) => {
-    const id = get(detailRequestProjectId);
-    const { data } = await API.get(`/requestprojects/id=${id}`);
-    return data;
+const getDetailRequestState = selector<User & RequestProjectType>({
+  key: "getDetailRequestState",
+  get: ({ get }) => {
+    // const id = get(detailRequestProjectIdState);
+    // const { data } = await API.get(`/requestprojects/?id=${id}`);
+    return {
+      userProfile: "",
+      userDetail: "test",
+      userEmail: "spiderq10",
+      requestProjectId: 1,
+      requestTitle: "test go",
+      requestContent: "test",
+      requestField: ["melody"],
+      requestGenre: ["Hiphop"],
+      requestMood: ["Happy"],
+      requestInstrument: "",
+      requestLyric: "",
+      requestMelody: "",
+      contributeList: [],
+    };
+
+    // return {
+    //   userProfile: data.userProfile,
+    //   userDetail: data.userDetail,
+    //   userEmail: data.email,
+    //   requestProjectId: data.requestProjectId,
+    //   requestTitle: data.requestTitle,
+    //   requestContent: data.requestContent,
+    //   requestField: data.requestField,
+    //   requestGenre: data.requestGenre,
+    //   requestMood: data.requestMood,
+    //   requestInstrument: data.requestInstrument,
+    //   requestLyric: data.requestLyric,
+    //   requestMelody: data.requestMelody,
+    // };
   },
 });
 
-export { detailRequestProjectId, getDetailRequest };
+const contributeListState = selector<Array<User & ContributeProject>>({
+  key: "contributeListState",
+  get: ({ get }) => {
+    const data = get(getDetailRequestState);
+    const contributeList = data.contributeList!;
+    return contributeList;
+  },
+});
+
+export {
+  detailRequestProjectIdState,
+  getDetailRequestState,
+  contributeListState,
+};
