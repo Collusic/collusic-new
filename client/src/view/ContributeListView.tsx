32ee581ccbdfeb80ac6215b30efd2328dc766878
spiderq2697 <spiderq10@gmail.com>
2022-01-25 13:51:07 +0900

    feat : 요청작 상세 페이지의 기여작 목록 View 추가

---
+++ client/src/view/ContributeListView.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { ContributeProject } from "../types/contributeProjectType";
+import { User } from "../types/userType";
+import { requestStates } from "../utils/requestStates";
+import "./contributeList.scss";
+
+type ContributeListProps = {
+  contributeList: Array<User & ContributeProject>;
+};
+
+export const ContributeListView: React.FC<ContributeListProps> = ({
+  contributeList,
+}) => {
+  return (
+    <article className="contributeList-box">
+      {contributeList.map((project, idx) => (
+        <section className="contribute-project" key={idx}>
+          <img
+            src={
+              project.userProfile !== ""
+                ? project.userProfile
+                : `../../assets/defaultProfile/defaultProfile.png`
+            }
+            alt={project.userEmail}
+            className="profile"
+          />
+          <section className="email">{project.userDetail}</section>
+          <section className="field">{project.contributeField}</section>
+          <section className="upload">
+            {project.contributeField.map((key, idx) => {
+              return requestStates(
+                idx,
+                project.contributeMelody!,
+                project.contributeInstrument!,
+                project.contributeLyric!
+              )[key];
+            })}
+          </section>
+        </section>
+      ))}
+    </article>
+  );
+};
