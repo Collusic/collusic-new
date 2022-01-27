5b11976696116589a3b903908ad9eb47e4caeaf3
sunghyuki <sunghyuk1609@gmail.com>
2022-01-28 01:33:51 +0900

    refactor : 기여작 리스트 조회 API 컨트롤러 메서드 삭제 - 기여작 리스트 조회는 요청작 상세페이지 조회 때 함께 조회되어야 함.

--- server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/RequestProjectApiController.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/RequestProjectApiController.java
@@ -1,10 +1,7 @@
 package com.collusic.collusicbe.web.controller;
 
 import com.collusic.collusicbe.service.RequestProjectService;
-import com.collusic.collusicbe.web.dto.RequestProjectResponseDto;
-import com.collusic.collusicbe.web.dto.RequestProjectSaveRequestDto;
-import com.collusic.collusicbe.web.dto.RequestProjectUpdateRequestDto;
-import com.collusic.collusicbe.web.dto.RequestProjectsWithPaginationDto;
+import com.collusic.collusicbe.web.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
