6f2098650727eddf25435546335dbcbdc3b2719c
spiderq2697 <spiderq10@gmail.com>
2022-07-03 20:06:47 +0900

    feat: 기존 개발 중이었던 도메인 연결을 중단한다
    - 기존 collusic의 기획에 있던 페이지 Router를 주석처리한다

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,25 +2,25 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { NavBarView } from "./view/NavBarView";
-import { MainPageView } from "./view/MainPageView";
-import { CreateRequestPageView } from "./view/CreateRequestPageView";
-import { DetailRequestPageView } from "./view/DetailRequestPageView";
+// import { NavBarView } from "./view/NavBarView";
+// import { MainPageView } from "./view/MainPageView";
+// import { CreateRequestPageView } from "./view/CreateRequestPageView";
+// import { DetailRequestPageView } from "./view/DetailRequestPageView";
 // import { CreateContributePageView } from "./view/CreateContributePageView";
 
 const App: React.FC = () => {
   return (
     <RecoilRoot>
       <BrowserRouter>
-        <NavBarView></NavBarView>
+        {/* <NavBarView></NavBarView> */}
         <Routes>
-          <Route path="/main/requestprojects" element={<MainPageView />} />
+          {/* <Route path="/main/requestprojects" element={<MainPageView />} />
           <Route path="/requestprojects" element={<CreateRequestPageView />} />
           <Route
             path="/requestprojects/:id"
             element={<DetailRequestPageView></DetailRequestPageView>}
           ></Route>
-          {/* <Route
+          <Route
             path="/requestprojects/:id/contributeprojects"
             element={<CreateContributePageView />}
           ></Route> */}
