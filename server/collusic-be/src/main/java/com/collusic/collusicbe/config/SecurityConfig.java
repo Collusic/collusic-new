3f44d7ed1e6c21ff644dfd4d65e457f8126ebd3c
sunghyuki <sunghyuk1609@gmail.com>
2022-07-10 23:01:12 +0900

    chore : 스프링 시큐리티 보일러 플레이팅 - 스프링 시큐리티 및 OAuth2.0 의존성 추가 - SecurityConfig 클래스 생성

---
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.collusic.collusicbe.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
