47ef60790f627cba340568ba706ef11a83c79f27
sunghyuki <sunghyuk1609@gmail.com>
2022-07-10 23:12:27 +0900

    feat : security config 설정 적용

--- server/collusic-be/src/main/java/com/collusic/collusicbe/config/SecurityConfig.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/config/SecurityConfig.java
@@ -1,8 +1,28 @@
 package com.collusic.collusicbe.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> customOAuthUserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .de
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuthUserService)
+    }
 }
