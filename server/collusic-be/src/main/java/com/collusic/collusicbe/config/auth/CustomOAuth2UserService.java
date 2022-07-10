131cc4fe1da08baac46cd1c37128ccbf0584ca46
sunghyuki <sunghyuk1609@gmail.com>
2022-07-11 01:27:01 +0900

    refactor : CustomOAuth2UserService 리펙토링 - OAuth2UserService 구현에서 DefaultOAuth2UserService 상속으로 변경

--- server/collusic-be/src/main/java/com/collusic/collusicbe/config/auth/CustomOAuth2UserService.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/config/auth/CustomOAuth2UserService.java
@@ -3,18 +3,16 @@ package com.collusic.collusicbe.config.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
-        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        OAuth2User oAuth2User = super.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration()
                                            .getRegistrationId();
