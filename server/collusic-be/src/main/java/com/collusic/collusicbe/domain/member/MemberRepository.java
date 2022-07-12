434c22db2fe7c651933e77cb6075f91b494cd50e
Bellroute <kjkun7631@gmail.com>
2022-07-12 22:51:26 +0900

    feat : MemberRepository 추가

---
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/domain/member/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.collusic.collusicbe.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
