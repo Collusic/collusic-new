7e81c36a08e55ad032c642acea9c7f4c2f9cb13f
Bellroute <kjkun7631@gmail.com>
2022-07-18 02:09:54 +0900

    fix: 동일한 타입의 빈 등록 클래스의 충돌 회피 처리

--- server/collusic-be/src/main/java/com/collusic/collusicbe/web/auth/OAuth2ProviderClientManager.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/auth/OAuth2ProviderClientManager.java
@@ -1,28 +1,38 @@
 package com.collusic.collusicbe.web.auth;
 
 import com.collusic.collusicbe.domain.member.SnsType;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OAuth2ProviderClientManager {
 
-    private final OAuth2ClientService googleClient;
-    private final OAuth2ClientService kakaoClient;
-    private final OAuth2ClientService naverClient;
+    @Qualifier("googleClientService")
+    private final OAuth2ClientService googleClientService;
+
+    @Qualifier("kakaoClientService")
+    private final OAuth2ClientService kakaoClientService;
+
+    @Qualifier("naverClientService")
+    private final OAuth2ClientService naverClientSerrvice;
+
+    public OAuth2ProviderClientManager(OAuth2ClientService googleClientService, OAuth2ClientService kakaoClientService, OAuth2ClientService naverClientSerrvice) {
+        this.googleClientService = googleClientService;
+        this.kakaoClientService = kakaoClientService;
+        this.naverClientSerrvice = naverClientSerrvice;
+    }
 
     public OAuth2ClientService getClientService(String provider) {
         provider = provider.toUpperCase();
 
         if (provider.equals(SnsType.NAVER.name())) {
-            return naverClient;
+            return naverClientSerrvice;
         }
 
         if (provider.equals(SnsType.KAKAO.name())) {
-            return kakaoClient;
+            return kakaoClientService;
         }
 
-        return googleClient;
+        return googleClientService;
     }
 }
