835d977d1d8a2e172a9857273c0200774e2a3177
sunghyuki <sunghyuk1609@gmail.com>
2022-07-18 23:25:58 +0900

    feat : OAuth2LoginResponseDto 추가 - 응답 타입 Enum으로 분류

--- server/collusic-be/src/main/java/com/collusic/collusicbe/web/auth/OAuth2Controller.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/auth/OAuth2Controller.java
@@ -1,21 +1,28 @@
 package com.collusic.collusicbe.web.auth;
 
+import com.collusic.collusicbe.domain.member.Member;
+import com.collusic.collusicbe.domain.member.MemberRepository;
+import com.collusic.collusicbe.domain.member.SnsType;
+import com.collusic.collusicbe.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class OAuth2Controller {
 
     private final OAuth2ProviderClientManager oAuth2ProviderClientManager;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/oauth2/login/{provider}")
-    public String loginToSns(@PathVariable String provider, @RequestParam Map<String, Object> authCode) {
+    public ResponseEntity<OAuth2LoginResponseDto> loginToSns(@PathVariable String provider, @RequestParam Map<String, Object> authCode) {
         OAuth2ClientService oAuth2Client = oAuth2ProviderClientManager.getClientService(provider);
         OAuth2Response response = oAuth2Client.requestLogin(authCode);
 
