cb0c4b3e2998f16f6dc965af569ac9071af81e82
Bellroute <kjkun7631@gmail.com>
2022-07-23 22:54:51 +0900

    feat: security config 설정 추가 - 인가 요청 범위 설정 - JwtAuthenticationFilter 및 JwtAuthenticationProvider 설정 추가 - spring boot starter oauth2 client 의존성 및 코드 제거

--- server/collusic-be/src/main/java/com/collusic/collusicbe/config/SecurityConfig.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/config/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.collusic.collusicbe.config;
 
+import com.collusic.collusicbe.config.auth.JWTAuthenticationFilter;
+import com.collusic.collusicbe.config.auth.JWTAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> customOAuthUserService;
+    private final JWTAuthenticationProvider jwtAuthenticationProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,12 +25,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin().disable()
-                .httpBasic().disable()
-                // TODO : authorizeRequests()
-                // TODO : logout()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuthUserService);
-                // TODO : success 시 JWT 처리
+                .authorizeRequests(request -> request.antMatchers(HttpMethod.POST, "/members").permitAll()
+                                                     .antMatchers(HttpMethod.GET, "/oauth2/login/**").permitAll()
+                                                     .anyRequest().authenticated())
+                .addFilterAt(new JWTAuthenticationFilter(authenticationManager()), BasicAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+           .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(jwtAuthenticationProvider);
     }
 }
