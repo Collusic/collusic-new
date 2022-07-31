b146d9612d42ddb41c77dbb811fb59282cc5c98c
gxxrxn <rbfks1221@naver.com>
2022-07-31 20:37:08 +0900

    feat: 모달 컴포넌트 리팩토링 후 추가정보 입력 모달창 구현

---
+++ client/src/model/signUpModel.ts
@@ -0,0 +1,18 @@
+import { atom, selector } from "recoil";
+
+export const signUpState = atom({
+  key: "signUpState",
+  default: false,
+});
+
+export const getSignUpUserInfo = selector({
+  key: "getLoginDetailInfo",
+  get: () => {
+
+    return {
+      profileSrc: "../../assets/signin/default_profile@2x.png",
+      email: "kkr991221@gmail.com",
+      nickName: "후라이"
+    }
+  },
+})
--- client/src/view/SignInView.tsx
+++ client/src/view/SignInView.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "../utils/style/login.scss";
 import { LoginButton } from "../components/LoginButton";
 
-export function LoginView() {
+type signInProps = {
+  onClickHandler(): void;
+};
+
+export function SignInView({ onClickHandler }: signInProps) {
   return (
     <div className="login-view">
       <LoginButton
@@ -11,18 +15,21 @@ export function LoginView() {
         backgroundColor="#03c75a"
         textColor="#fff"
         src="../assets/login/naver.svg"
+        onClickHandler={onClickHandler}
       />
       <LoginButton
         innerText="카카오로 시작하기"
         backgroundColor="#fee500"
         textColor="#000"
         src="../assets/login/kakao.svg"
+        onClickHandler={onClickHandler}
       />
       <LoginButton
         innerText="구글로 시작하기"
         backgroundColor="#fff"
         textColor="#000"
         src="../assets/login/google.svg"
+        onClickHandler={onClickHandler}
       />
     </div>
   );
