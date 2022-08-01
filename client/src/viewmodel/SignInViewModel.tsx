791d6550280a192a5ba20619c4d4bf9fef07255d
spiderq2697 <spiderq10@gmail.com>
2022-08-01 17:10:47 +0900

    feat: sns 로그인/회원가입 기능을 구현한다
    - oauth 서버로부터 authorization code를 받아 server에 data를 요청한다
    - responseType에 따른 로그인, 회원가입, 에러 로직을 구현한다

--- client/src/viewmodel/SignInViewModel.tsx
+++ client/src/viewmodel/SignInViewModel.tsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 
 import { SignInView } from "../view/SignInView";
-
-import { modalOpenState } from "../model/signInModel";
-import { signUpState } from "../model/signUpModel";
+import { loginTypeState } from "../model/userModel";
 
 export function SignInViewModel() {
-  const navigate = useNavigate();
-  const setModalOpenSate = useSetRecoilState(modalOpenState);
-  const setSignUpState = useSetRecoilState(signUpState);
+  const [loginType, setLoginType] = useRecoilState(loginTypeState);
 
-  const onClickHandler = () => {
-    setModalOpenSate(false);
-    setSignUpState(true);
-    navigate("/signup");
+  const naverClickHandler = () => {
+    setLoginType("naver");
+    window.location.href =
+      "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=37fxHiyXMIr6sxLm2MPe&scope=email&state=7IiQorH-lspV_Axq3aD2sSvI5bAVYVB1RY3R2o70Ilo%3D&redirect_uri=http://localhost:3000/oauth/redirect";
   };
 
-  return <SignInView onClickHandler={onClickHandler} />;
+  const kakaoClickHandler = () => {
+    setLoginType("kakao");
+    console.log(loginType);
+    window.location.href =
+      "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=6acab7ac79ce0a072e6ddf08861d28ed&scope=account_email openid&state=qYXwGnJBNMGJPabC1nmKuMn9CChc3WCF72mmr8HHRi8%3D&redirect_uri=http://localhost:3000/auth/redirect";
+  };
+  const googleClickHandler = () => {
+    setLoginType("google");
+    window.location.href =
+      "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=717532271001-pu3fqqghv2rbi3ldeivht3pmlvaveep6.apps.googleusercontent.com&scope=emailopenid&state=vGT5WolwVcBKgizeawrLfpjUfxiVzE-RmJnioZGWIBI%3D&redirect_uri=http://localhost:3000/auth/redirect";
+  };
+
+  return (
+    <SignInView
+      naverClickHandler={naverClickHandler}
+      kakaoClickHandler={kakaoClickHandler}
+      googleClickHandler={googleClickHandler}
+    />
+  );
 }
