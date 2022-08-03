69c6342926301212c72efcecf23f67535ca1751f
sunghyuki <sunghyuk1609@gmail.com>
2022-08-03 16:46:07 +0900

    feat : 회원가입 요청 Dto Validation 어노테이션 추가

--- server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/dto/SignUpRequestDto.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/web/controller/dto/SignUpRequestDto.java
@@ -6,14 +6,27 @@ import com.collusic.collusicbe.domain.member.SnsType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Getter
 @NoArgsConstructor
 public class SignUpRequestDto {
 
+    @Email
     private String email;
+
+    @NotBlank
     private String authId;
+
+    @NotBlank
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]", message = "닉네임에 특수문자를 포함할 수 없습니다.")
+    @Size(min = 2, max = 24, message = "닉네임은 2 ~ 24자 이내여야 합니다.")
     private String nickName;
+
+    @NotBlank
     private String profileImageUrl;
+
+    @NotBlank
     private SnsType snsType;
 
     public SignUpRequestDto(String email, String authId, String nickName, String profileImageUrl, String snsType) {
