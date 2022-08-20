148fa36cb98af3ee338ef914f3b4d01a5bcd6ab5
gxxrxn <rbfks1221@naver.com>
2022-08-20 21:46:01 +0900

    fix: sns type에 따른 로그인 기능 구현

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -7,6 +7,13 @@ export const API = axios.create({
   timeout: 1000,
 });
 
+export const LOCAL_API = axios.create({
+  baseURL: "http://localhost:8080/",
+  headers: { "X-Custom-Header": "foobar" },
+  withCredentials: true,
+  timeout: 2000,
+});
+
 export const TEST_API = axios.create({
   baseURL: "http://ec2-13-124-123-6.ap-northeast-2.compute.amazonaws.com:8080/",
   headers: {
--- client/src/viewmodel/RedirectViewModel.tsx
+++ client/src/viewmodel/RedirectViewModel.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { useParams, useSearchParams, useNavigate } from "react-router-dom";
 import { AxiosResponse } from "axios";
 
-import { API } from "../utils/axios";
-import { loginTypeState, refreshTokenState } from "../model/userModel";
+import { LOCAL_API } from "../utils/axios";
+import { refreshTokenState } from "../model/userModel";
 import { modalOpenState } from "../model/signInModel";
 import { signUpState } from "../model/signUpModel";
 
@@ -12,31 +12,33 @@ export function RedirectViewModel() {
   const setModalOpenState = useSetRecoilState(modalOpenState);
   const setSignUpState = useSetRecoilState(signUpState);
   const setRefreshToken = useSetRecoilState(refreshTokenState);
-  const loginType = useRecoilValue(loginTypeState);
+
   const navigate = useNavigate();
+  const { provider } = useParams();
   const [query] = useSearchParams();
-  const authCode = query.get("code");
+  const code = query.get("code");
   const state = query.get("state");
 
   useEffect(() => {
     const getLoginState = async () => {
       try {
-        const { data }: AxiosResponse = await API.get(`/oauth2/login/kakao`, {
-          data: {
-            auth_code: (authCode as string) + state,
+        const { data }: AxiosResponse = await LOCAL_API.get(`/oauth2/login/${provider}`, {
+          params: {
+            code,
+            state,
           },
         });
 
         if (data.responseType === "SIGN_IN") {
           const { accessToken, refreshToken } = data;
           setRefreshToken(refreshToken);
-          API.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+          LOCAL_API.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
         }
 
         if (data.responseType === "SIGN_UP") {
           setModalOpenState(false);
           setSignUpState(true);
-          navigate("/signup", { state: data });
+          navigate("/signup", { state: data.attributes });
         }
 
         if (data.responseType === "ERROR") {
@@ -49,7 +51,7 @@ export function RedirectViewModel() {
     };
 
     getLoginState();
-  }, [loginType]);
+  });
 
-  return <div>{loginType} 로그인 로딩중</div>;
+  return <div>{provider} 로그인 로딩중</div>;
 }
