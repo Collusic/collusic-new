e296197eb9b65d3e1dcdd4a83a2b3490014319b1
spiderq2697 <spiderq10@gmail.com>
2022-09-10 16:22:51 +0900

    feat: mainButton을 구현한다

---
+++ client/src/components/MainButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "utils/style/mainButton.scss";
+
+function MainButton() {
+  return <button type="button">BUTTON</button>;
+}
+
+export default MainButton;
---
+++ client/src/stories/mainButton.stories.tsx
@@ -0,0 +1,12 @@
+import MainButton from "components/MainButton";
+
+export default {
+  title: "Button/MainButton",
+  component: MainButton,
+};
+
+function Template() {
+  return <MainButton />;
+}
+
+export const Naver = Template.bind({});
