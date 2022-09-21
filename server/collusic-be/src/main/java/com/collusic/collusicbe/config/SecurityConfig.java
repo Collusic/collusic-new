144b228647959d721a4ae9506efa7f225d823d60
sunghyuki <sunghyuk1609@gmail.com>
2022-09-21 19:50:25 +0900

    feat : 리프레시 토큰을 통한 액세스 토큰 재발급 기능 추가

--- server/collusic-be/src/main/java/com/collusic/collusicbe/config/SecurityConfig.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.collusic.collusicbe.config;
 
 import com.collusic.collusicbe.config.auth.JWTAuthenticationFilter;
 import com.collusic.collusicbe.config.auth.JWTAuthenticationProvider;
+import com.collusic.collusicbe.service.TokenService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +24,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JWTAuthenticationProvider jwtAuthenticationProvider;
+    private final TokenService tokenService;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -34,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                                      .antMatchers("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs").permitAll()
                                                      .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                                                      .anyRequest().authenticated())
-                .addFilterAt(new JWTAuthenticationFilter(authenticationManager()), BasicAuthenticationFilter.class);
+                .addFilterAt(new JWTAuthenticationFilter(authenticationManager(), tokenService, objectMapper), BasicAuthenticationFilter.class);
     }
 
     @Bean
