7b39eaec36c24170e950d3bd58596cd432c58ac7
spiderq2697 <spiderq10@gmail.com>
2022-09-27 15:18:58 +0900

    refactor: button 컴포넌트의 width와 marginLeft를 수정할 수 있다

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -9,22 +9,33 @@ type ColorType = "green" | "white" | "text" | "line";
 interface ButtonProps {
   type: ColorType;
   isSelected: boolean;
-  width: string;
   clickHandler: MouseEventHandler;
   children: ReactNode;
+  width?: string;
+  marginLeft?: string;
 }
 
-function Button({ type, isSelected, width, clickHandler, children }: ButtonProps) {
+const defaultProps = {
+  width: "fit-content",
+  marginLeft: "0",
+};
+
+function Button({ type, isSelected, clickHandler, children, width, marginLeft }: ButtonProps) {
   return (
     <button
       type="button"
       className={classNames(type, { selected: isSelected })}
       onClick={clickHandler}
-      style={{ width }}
+      style={{
+        width,
+        marginLeft,
+      }}
     >
       {children}
     </button>
   );
 }
+
+Button.defaultProps = defaultProps;
 
 export default Button;
