b6984376ae61812324aad52d661afded31c3c85a
spiderq2697 <spiderq10@gmail.com>
2022-10-16 16:52:46 +0900

    refactor: slider position을 absolute로 변경한다

--- client/src/components/blocks/Bpm/index.tsx
+++ client/src/components/blocks/Bpm/index.tsx
@@ -13,10 +13,11 @@ function Bpm({ bpmState, inputHandler }: BpmProps) {
   const [bpm, setBpm] = useState(bpmState || 0);
   const sliderRef = useRef<HTMLInputElement>(null);
   const sliderValueRef = useRef<HTMLOutputElement>(null);
+  const bpmFillRef = useRef<HTMLDivElement>(null);
 
   // todo: 현재 bpm 알려주는 span 태그 클릭 시 sliderHandle이 동작되지 않는 에러
-  // todo: 현재 bpm 만큼 진행도 흰색배경으로 표시
-
+  // todo: 진행된 bpm 상태바 클릭 시 작동 안되는 에러
+  
   const formHandler = (e: FormEvent) => {
     setBpm(Number((e.target as HTMLInputElement).value));
     inputHandler(e);
@@ -26,12 +27,20 @@ function Bpm({ bpmState, inputHandler }: BpmProps) {
     (sliderRef.current as HTMLInputElement).value = String(bpm);
   });
 
+  // todo: 중복 제거
   useEffect(() => {
     (sliderValueRef.current as HTMLOutputElement).style.left = getSliderHandlePosition(
-      0,
+      30,
       240,
       bpm,
-      Number(sliderRef.current?.getBoundingClientRect().width) - 40,
+      Number(sliderRef.current?.getBoundingClientRect().width),
+    );
+
+    (bpmFillRef.current as HTMLDivElement).style.width = getSliderHandlePosition(
+      30,
+      240,
+      bpm,
+      Number(sliderRef.current?.getBoundingClientRect().width),
     );
   }, [bpm]);
 
@@ -42,14 +51,15 @@ function Bpm({ bpmState, inputHandler }: BpmProps) {
         <input
           className="slider"
           type="range"
-          min="0"
+          min="30"
           max="240"
           step="1"
           name="bpm"
           onInput={formHandler}
           ref={sliderRef}
         />
-        <output id="value" name="value" htmlFor="bpm" ref={sliderValueRef}>
+        <div className="bpm-fill" ref={bpmFillRef} />
+        <output id="output" name="output" htmlFor="bpm" ref={sliderValueRef}>
           {bpm}
         </output>
         <div className="slider-bottom">
--- client/src/utils/slider.ts
+++ client/src/utils/slider.ts
@@ -1,5 +1,5 @@
 export const getSliderHandlePosition = (min: number, max: number, value: number, sliderWidth: number) => {
   const percent = (value - min) / (max - min);
-
-  return `${sliderWidth * percent + 20}px`;
+  console.log(sliderWidth, percent);
+  return `${20 + (sliderWidth - 40) * percent}px`;
 };
