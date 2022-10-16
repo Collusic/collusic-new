36d573363c9cb005c6a784e0e046c8adceb3ed58
spiderq2697 <spiderq10@gmail.com>
2022-10-16 15:19:33 +0900

    refactor: bpm 컴포넌트에서 뷰로직만 관리한다
    - 실시간으로 현재 설정 중인 bpm을 state로 관리하여 보여주고 recoilState를 추가로 관리한다

--- client/src/components/blocks/Bpm/index.tsx
+++ client/src/components/blocks/Bpm/index.tsx
@@ -1,24 +1,57 @@
-import React, { FormEventHandler } from "react";
+import React, { FormEvent, FormEventHandler, useEffect, useRef, useState } from "react";
 
 import Span from "components/atoms/Span";
+import { getSliderHandlePosition } from "utils/slider";
 import "./style.scss";
 
 interface BpmProps {
-  bpm: number;
+  bpmState: number;
   inputHandler: FormEventHandler;
-  sliderRef: React.RefObject<HTMLInputElement>;
-  sliderValueRef: React.RefObject<HTMLSpanElement>;
 }
 
-function Bpm({ bpm, inputHandler, sliderRef, sliderValueRef }: BpmProps) {
+function Bpm({ bpmState, inputHandler }: BpmProps) {
+  const [bpm, setBpm] = useState(bpmState || 0);
+  const sliderRef = useRef<HTMLInputElement>(null);
+  const sliderValueRef = useRef<HTMLOutputElement>(null);
+
+  // todo: 현재 bpm 알려주는 span 태그 클릭 시 sliderHandle이 동작되지 않는 에러
+  // todo: 현재 bpm 만큼 진행도 흰색배경으로 표시
+
+  const formHandler = (e: FormEvent) => {
+    setBpm(Number((e.target as HTMLInputElement).value));
+    inputHandler(e);
+  };
+
+  useEffect(() => {
+    (sliderRef.current as HTMLInputElement).value = String(bpm);
+  });
+
+  useEffect(() => {
+    (sliderValueRef.current as HTMLOutputElement).style.left = getSliderHandlePosition(
+      0,
+      240,
+      bpm,
+      Number(sliderRef.current?.getBoundingClientRect().width) - 40,
+    );
+  }, [bpm]);
+
   return (
     <div id="bpm">
       <Span>BPM</Span>
       <div className="slide-container">
-        <input className="slider" type="range" min="0" max="240" step="1" onInput={inputHandler} ref={sliderRef} />
-        <span id="value" ref={sliderValueRef}>
+        <input
+          className="slider"
+          type="range"
+          min="0"
+          max="240"
+          step="1"
+          name="bpm"
+          onInput={formHandler}
+          ref={sliderRef}
+        />
+        <output id="value" name="value" htmlFor="bpm" ref={sliderValueRef}>
           {bpm}
-        </span>
+        </output>
         <div className="slider-bottom">
           <span>30</span>
           <span>240</span>
--- client/src/viewmodel/ProjectViewModel.tsx
+++ client/src/viewmodel/ProjectViewModel.tsx
@@ -1,36 +1,18 @@
-import React, { FormEvent, useEffect, useRef } from "react";
+import React, { FormEvent } from "react";
 import { useRecoilState } from "recoil";
 
 import { bpmState } from "model/projectModel";
-import { getSliderHandlePosition } from "utils/slider";
 import Bpm from "components/blocks/Bpm";
 
 function ProjectViewModel() {
   const [bpm, setBpm] = useRecoilState(bpmState);
-  const sliderRef = useRef<HTMLInputElement>(null);
-  const sliderValueRef = useRef<HTMLSpanElement>(null);
 
   const inputHandler = (e: FormEvent) => {
+    // todo: setBpm 디바운싱 처리
     setBpm(Number((e.target as HTMLInputElement).value));
-
-    (sliderValueRef.current as HTMLSpanElement).style.left = getSliderHandlePosition(
-      0,
-      240,
-      Number((e.target as HTMLInputElement).value),
-      Number(sliderRef.current?.getBoundingClientRect().width) - 40,
-    );
   };
 
-  useEffect(() => {
-    (sliderValueRef.current as HTMLSpanElement).style.left = getSliderHandlePosition(
-      0,
-      240,
-      Number(sliderRef.current!.value),
-      Number(sliderRef.current?.getBoundingClientRect().width) - 40,
-    );
-  }, []);
-
-  return <Bpm bpm={bpm} inputHandler={inputHandler} sliderRef={sliderRef} sliderValueRef={sliderRef} />;
+  return <Bpm bpmState={bpm} inputHandler={inputHandler} />;
 }
 
 export default ProjectViewModel;
