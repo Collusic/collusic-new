5678f7d8a7a56cf63997fe61b8e88f4bb9a7aa30
spiderq2697 <spiderq10@gmail.com>
2022-10-27 21:08:35 +0900

    feat: track tag 컴포넌트를 구현한다
    - 선택된 트랙 이름과 track의 이름이 같을 경우, 선택된 버튼으로 표시한다

---
+++ client/src/stories/TrackTag.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import TrackTag from "components/blocks/TrackTag";
+
+export default {
+  title: "blocks/TrackTag",
+  component: TrackTag,
+  argTypes: {
+    selectedTrack: { control: "text" },
+  } as ComponentMeta<typeof TrackTag>,
+};
+
+const Template: ComponentStory<typeof TrackTag> = (args) => <TrackTag {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  handleBtnClick: (e) => {
+    console.log(e);
+  },
+  tracks: ["피아노", "드럼", "보컬", "박수", "어쿠스틱 기타", "일렉 기타", "바이올린", "리코더", "마라카스", "ETC"],
+};
