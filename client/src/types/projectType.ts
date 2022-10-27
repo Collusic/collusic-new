2445ac5c2a6611d31c6210fecabc0100f21bda36
spiderq2697 <spiderq10@gmail.com>
2022-10-27 21:05:57 +0900

    refactor: 트랙 타입을 한글로 변경한다

--- client/src/types/projectType.ts
+++ client/src/types/projectType.ts
@@ -1,13 +1,13 @@
 export type Track =
-  | "piano"
-  | "drum"
-  | "vocal"
-  | "crap"
-  | "acousticguitar"
-  | "electricguitar"
-  | "violin"
-  | "recorder"
-  | "maracas"
-  | "etc";
+  | "피아노"
+  | "드럼"
+  | "보컬"
+  | "박수"
+  | "어쿠스틱 기타"
+  | "일렉 기타"
+  | "바이올린"
+  | "리코더"
+  | "마라카스"
+  | "ETC";
 
 export type MelodyLength = "4" | "8" | "16";
