4be428d1fe39e7949b46e67e2dfae40709783191
spiderq2697 <spiderq10@gmail.com>
2022-10-27 21:15:31 +0900

    refactor: clickHandler 이름을 명확하게 변경한다

--- client/src/components/blocks/SessionMelody/index.tsx
+++ client/src/components/blocks/SessionMelody/index.tsx
@@ -7,21 +7,21 @@ import "./style.scss";
 
 interface SessionMelodyProps {
   melodyType: MelodyLength;
-  clickHandler: MouseEventHandler;
+  handleBtnClick: MouseEventHandler;
 }
 
-function SessionMelody({ melodyType, clickHandler }: SessionMelodyProps) {
+function SessionMelody({ melodyType, handleBtnClick }: SessionMelodyProps) {
   return (
     <div className="session-melody">
       <Span>세션 마디</Span>
       <div className="session-buttons">
-        <Button type="line" isSelected={melodyType === "4"} clickHandler={clickHandler} width="6rem">
+        <Button type="line" isSelected={melodyType === "4"} onBtnClick={handleBtnClick} width="6rem">
           4마디
         </Button>
-        <Button type="line" isSelected={melodyType === "8"} clickHandler={clickHandler} width="8rem" marginLeft="8px">
+        <Button type="line" isSelected={melodyType === "8"} onBtnClick={handleBtnClick} width="8rem" marginLeft="8px">
           8마디
         </Button>
-        <Button type="line" isSelected={melodyType === "16"} clickHandler={clickHandler} width="10rem" marginLeft="8px">
+        <Button type="line" isSelected={melodyType === "16"} onBtnClick={handleBtnClick} width="10rem" marginLeft="8px">
           16마디
         </Button>
       </div>
