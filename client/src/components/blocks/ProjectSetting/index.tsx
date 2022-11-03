9ead9679fa0796df89574d0e87ae0665da1b6836
spiderq2697 <spiderq10@gmail.com>
2022-11-03 21:42:51 +0900

    feat: ProjectSetting 컴포넌트에 atom, block 컴포넌트를 넣는다

---
+++ client/src/components/blocks/ProjectSetting/index.tsx
@@ -0,0 +1,53 @@
+import React, { FormEventHandler, KeyboardEventHandler, MouseEventHandler } from "react";
+
+import { MelodyLength, Track } from "types/projectType";
+import Button from "components/atoms/Button";
+import Toggle from "components/atoms/Toggle";
+import Bpm from "../Bpm";
+import RecordDevice from "../RecordDevice";
+import SessionMelody from "../SessionMelody";
+import TrackTag from "../TrackTag";
+import "./style.scss";
+
+interface ProjectSettingProps {
+  onDeviceClick: MouseEventHandler<HTMLUListElement> | KeyboardEventHandler<HTMLUListElement>;
+  onSessionMelodyClick: MouseEventHandler;
+  onTrackClick: MouseEventHandler;
+  onToggleClick: MouseEventHandler;
+  onBtnClick: MouseEventHandler;
+  onBpmInput: FormEventHandler;
+  melodyType: MelodyLength;
+  bpmState: number;
+  selectedTrack: Track;
+  tracks: Track[];
+  isLocked: boolean;
+  isSelected: boolean;
+}
+
+function ProjectSetting({
+  onDeviceClick,
+  onSessionMelodyClick,
+  onTrackClick,
+  onToggleClick,
+  onBtnClick,
+  onBpmInput,
+  melodyType,
+  bpmState,
+  selectedTrack,
+  tracks,
+  isLocked,
+  isSelected,
+}: ProjectSettingProps) {
+  return (
+    <div id="project-setting">
+      <RecordDevice onDeviceClick={onDeviceClick} />
+      <SessionMelody melodyType={melodyType} onSessionMelodyClick={onSessionMelodyClick} />
+      <Bpm bpmState={bpmState} onBpmInput={onBpmInput} />
+      <TrackTag onTrackClick={onTrackClick} selectedTrack={selectedTrack} tracks={tracks} />
+      <Toggle isLocked={isLocked} onToggleClick={onToggleClick} />
+      <Button type="green" isSelected={isSelected} onBtnClick={onBtnClick} />
+    </div>
+  );
+}
+
+export default ProjectSetting;
---
+++ client/src/stories/ProjectSetting.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory } from "@storybook/react";
+
+import ProjectSetting from "components/blocks/ProjectSetting";
+
+export default {
+  title: "blocks/ProjectSetting",
+  component: ProjectSetting,
+};
+
+const Template: ComponentStory<typeof ProjectSetting> = (args) => <ProjectSetting {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  onDeviceClick: () => {},
+  onSessionMelodyClick: () => {},
+  onTrackClick: () => {},
+  onToggleClick: () => {},
+  onBtnClick: () => {},
+  onBpmInput: () => {},
+  tracks: ["피아노", "드럼", "보컬", "박수", "어쿠스틱 기타", "일렉 기타", "바이올린", "리코더", "마라카스", "ETC"],
+};
