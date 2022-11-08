fb6df7e90ad386a645aaec25d882677634d43810
sunghyuki <sunghyuk1609@gmail.com>
2022-11-09 00:53:49 +0900

    refactor : 쿠키 만료 수정

--- server/collusic-be/src/main/java/com/collusic/collusicbe/config/auth/ExceptionHandlerFilter.java
+++ server/collusic-be/src/main/java/com/collusic/collusicbe/config/auth/ExceptionHandlerFilter.java
@@ -8,13 +8,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -24,7 +24,6 @@ import java.io.IOException;
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     private final ObjectMapper objectMapper;
-    private final CookieClearingLogoutHandler cookieClearingLogoutHandler;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -32,10 +31,16 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         try {
             filterChain.doFilter(request, response);
         } catch (ExpiredJwtException | AbnormalAccessException | EntityNotFoundException | UnAuthorizedException e) {
-            cookieClearingLogoutHandler.logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+            expireCookie(response, "refreshToken");
             SecurityContextHolder.clearContext();
             sendErrorResponse(HttpStatus.UNAUTHORIZED.value(), response, e);
         }
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 
     private void sendErrorResponse(int status, HttpServletResponse response, RuntimeException e) throws IOException {
