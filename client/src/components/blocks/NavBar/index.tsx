4e68c8c913876fc99f45d565e4e26a7c1866c285
spiderq2697 <spiderq10@gmail.com>
2022-12-09 00:30:27 +0900

    feat: 네비게이션 바를 생성한다

--- client/src/components/blocks/NavBar/index.tsx
+++ client/src/components/blocks/NavBar/index.tsx
@@ -1,33 +1,28 @@
 import React from "react";
 import { useRecoilState } from "recoil";
 
-import { modalOpenState } from "../../../model/signInModel";
-import { SignInViewModel } from "../../../viewmodel/SignInViewModel";
-import { Modal } from "../../atoms/Modal";
+import { modalOpenState } from "model/signInModel";
+import { SignInViewModel } from "viewmodel/SignInViewModel";
+import { Modal } from "components/atoms/Modal";
 
 import "./style.scss";
 
 export function NavBar() {
   const [isModalOpen, setIsModalOpen] = useRecoilState(modalOpenState);
+  const LogoImgUrl = `${process.env.PUBLIC_URL}/assets/logo/logo@2x.png`;
+  const SignInImgUrl = `${process.env.PUBLIC_URL}/assets/signin/logo@2x.png`;
 
   return (
     <header>
       <div className="navbar">
-        <h1>
-          <a className="logo" href="/">
-            Collusic
-          </a>
-        </h1>
+        <a href="/">
+          <img src={LogoImgUrl} alt="" />
+        </a>
         <nav>
           <ul>
-            <li>
-              <a href="/">
-                <img src="../../../../public" alt="alarm" />
-              </a>
-            </li>
             <li className="signin">
-              <button className="signin-btn" type="button" onClick={() => setIsModalOpen(!isModalOpen)}>
-                로그인/회원가입
+              <button type="button" onClick={() => setIsModalOpen(!isModalOpen)}>
+                로그인 / 회원가입
               </button>
             </li>
           </ul>
@@ -35,7 +30,7 @@ export function NavBar() {
       </div>
       {isModalOpen ? (
         <Modal showModal={isModalOpen} setShowModal={setIsModalOpen}>
-          <img width="100%" src="../../assets/signin/logo@2x.png" alt="logo" />
+          <img width="100%" src={SignInImgUrl} alt="logo" />
           <SignInViewModel />
         </Modal>
       ) : null}
