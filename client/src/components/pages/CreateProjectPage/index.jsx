04eaec1df13f3eaa54079605bd1e9821edd56695
spiderq2697 <spiderq10@gmail.com>
2022-12-19 20:35:42 +0900

    feat: 프로젝트 생성페이지를 구현한다

---
+++ client/src/components/pages/CreateProjectPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style.scss";
+import ProjectSettingViewModel from "viewmodel/ProjectSettingViewModel";
+
+function CreateProjectPage() {
+  return (
+    <div id="create-project">
+      <ProjectSettingViewModel />
+    </div>
+  );
+}
+
+export default CreateProjectPage;
