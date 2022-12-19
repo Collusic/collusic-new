35b7b470fb4bfc01e1b99749f322389ad5ee068f
gxxrxn <rbfks1221@naver.com>
2022-12-19 19:37:35 +0900

    fix: root 라우팅 시 AuthVerify 수행하지 않도록 수정
    - eslint.js 에러 해결
    - npm install 실행

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     "no-shadow": 0,
     "react/prop-types": 0,
     "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
-    "no-unused-vars": null,
+    "no-unused-vars": 0,
     "jsx-a11y/no-noninteractive-element-interactions": 0,
   },
 };
