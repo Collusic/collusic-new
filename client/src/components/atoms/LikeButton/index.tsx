af6200a0b85f98fb4e541475fb69a3fde1d195a5
spiderq2697 <spiderq10@gmail.com>
2022-12-19 20:39:08 +0900

    feat: 좋아요 버튼을 구현한다

---
+++ client/src/components/atoms/LikeButton/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./style.scss";
+
+interface LikeButtonProps {
+  isLiked: boolean;
+  likeCount: number;
+  onClickLikeBtn: () => {};
+}
+
+function LikeButton({ isLiked, likeCount, onClickLikeBtn }: LikeButtonProps) {
+  return (
+    <button type="button" id="like-button" onClick={onClickLikeBtn}>
+      <img src={`${process.env.PUBLIC_URL}/assets/likeIcon/${isLiked ? "like.png" : "unlike.png"}`} alt="" />
+      <span>{likeCount}</span>
+    </button>
+  );
+}
+
+export default LikeButton;
