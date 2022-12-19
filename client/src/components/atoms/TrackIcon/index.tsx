9f58d2d6a9d135e942870bf559ffd991be874dd4
spiderq2697 <spiderq10@gmail.com>
2022-12-19 20:40:06 +0900

    feat: 트랙 아이콘을 구현한다

--- client/src/components/atoms/TrackIcon/index.tsx
+++ client/src/components/atoms/TrackIcon/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 
-import { TrackIconSrc } from "../../../utils/data/track";
-import { Track } from "../../../types/projectType";
+import { TrackIconSrc } from "utils/data/track";
+import { Track } from "types/projectType";
 import "./style.scss";
 
 interface TrackIconProps {
   track: Track;
+  style: {};
 }
-function TrackIcon({ track }: TrackIconProps) {
+function TrackIcon({ track, style }: TrackIconProps) {
   return (
-    <div id="track-icon">
+    <div id="track-icon" style={style}>
       <img src={TrackIconSrc[track]} alt={track} />
     </div>
   );
