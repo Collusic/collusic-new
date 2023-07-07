54778512ac94f013d0cf3e2f3cbe45caa1903438
spiderq2697 <spiderq10@gmail.com>
2023-07-08 00:20:18 +0900

    chore: msw 스크립트 적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import { RecoilRoot } from "recoil";
 import { worker } from "./mocks/browser";
 import App from "./App";
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.REACT_APP_MOCK === "Y") {
   worker.start();
 }
 
