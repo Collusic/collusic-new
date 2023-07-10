942237a8152b6b4f8225f839b2908f75745929bf
gxxrxn <rbfks1221@naver.com>
2023-07-10 15:25:31 +0900

    feat: useTimer hooks 구현

---
+++ client/src/hooks/useTimer.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+
+const useTimer = (value: number) => {
+  const [time, setTime] = useState(value);
+  const [isStart, setIsStart] = useState(false);
+  const [isEnd, setIsEnd] = useState(false);
+  const timerId = useRef<NodeJS.Timer>();
+
+  useEffect(() => {
+    if (isStart) {
+      setIsEnd(false);
+
+      timerId.current = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(timerId.current);
+  }, [isStart]);
+
+  useEffect(() => {
+    if (time <= 0) {
+      setIsEnd(true);
+      clearInterval(timerId.current);
+      setIsStart(false);
+      setTime(value);
+    }
+  }, [time]);
+
+  return { isEnd, time, startTimer: () => setIsStart(true) };
+};
+
+export default useTimer;
