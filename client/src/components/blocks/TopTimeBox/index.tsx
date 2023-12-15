5744951f164f9adcbe7ebac36addb1c0528bbdc0
spiderq2697 <spiderq10@gmail.com>
2023-12-15 21:44:59 +0900

    [Issue-387] fix: 브라우저 크기 변경 또는 초기 페이지 접근 시 상단 막대 간격이 정상적으로 설정되지 않는 이슈 해결

--- client/src/components/blocks/TopTimeBox/index.tsx
+++ client/src/components/blocks/TopTimeBox/index.tsx
@@ -1,18 +1,23 @@
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 import TopTimeBar from "components/atoms/TopTimeBar";
 import "./style.scss";
 
-function TopTimeBox({ bpm = 0 }) {
-  const topTimeBoxRef = useRef<HTMLDivElement>(null);
-  const stickCnt = Math.floor(bpm / 2) + 1;
+function TopTimeBox({ bpm = 30 }) {
+  const [stickCnt, setStickCnt] = useState(bpm);
+  const [timeBoxSize, setTimeBoxSize] = useState(
+    (document.querySelector("#track-space") as HTMLDivElement)?.offsetWidth,
+  );
 
   const gapBetweenBars = (isLast = false) => {
+    console.log("between");
     const WIDTH_OF_TIME_BAR = 4;
     const PADDING_LEFT = 4;
     const WIDTH_OF_PLAY_STICK = 12;
-    if (!topTimeBoxRef.current) return 0;
+
+    const trackSpaceDOM = document.querySelector("#track-space");
+    if (!trackSpaceDOM) return 0;
     const timeBoxWidth =
-      (document.querySelector("#track-space") as HTMLDivElement).offsetWidth -
+      (trackSpaceDOM as HTMLDivElement).offsetWidth -
       parseFloat(getComputedStyle(document.querySelector("#track-space") as HTMLDivElement).paddingLeft) -
       PADDING_LEFT -
       WIDTH_OF_PLAY_STICK;
@@ -23,8 +28,24 @@ function TopTimeBox({ bpm = 0 }) {
     return parseFloat((timeBoxWidth / (stickCnt - 1) - WIDTH_OF_TIME_BAR).toFixed(2));
   };
 
+  // 브라우저 크기가 변경될 경우, 상단 막대 간격 조정
+  useEffect(() => {
+    window.addEventListener("resize", () =>
+      setTimeBoxSize((document.querySelector("#track-space") as HTMLDivElement)?.offsetWidth),
+    );
+    return () => {
+      window.removeEventListener("resize", () =>
+        setTimeBoxSize((document.querySelector("#track-space") as HTMLDivElement)?.offsetWidth),
+      );
+    };
+  }, []);
+  // bpm이 변경될 경우, 상단 막대 간격 조정
+  useEffect(() => {
+    setStickCnt(Math.floor(bpm / 2) + 1);
+  }, [bpm, timeBoxSize]);
+
   return (
-    <div id="top-time-box" ref={topTimeBoxRef}>
+    <div id="top-time-box">
       {Array.from({ length: stickCnt }, (value, idx) => {
         if (idx % 4 === 0)
           return (
