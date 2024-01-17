8e85603966a6f0d2a786884e159db79ac285ed11
gxxrxn <rbfks1221@naver.com>
2024-01-17 18:35:51 +0900

    feat: 녹음 시 하단 재생버튼 비활성화

--- client/src/viewmodel/UnderPlayBarViewModel.tsx
+++ client/src/viewmodel/UnderPlayBarViewModel.tsx
@@ -6,10 +6,16 @@ import useAudios from "hooks/useAudios";
 
 import UnderPlayBar from "components/blocks/UnderPlayBar";
 
-function UnderPlayBarViewModel() {
+function UnderPlayBarViewModel({ isRecording }: { isRecording?: boolean }) {
   const { isAudioPlaying, toggleAudio, chanegeAudioVolume } = useAudios();
   const time = useRecoilValue(trackTimeState);
   const refinedTime = `00:${Math.floor(time).toString().padStart(2, "0")}`;
+
+  const handlePlayButtonClick = () => {
+    if (!isRecording) {
+      toggleAudio();
+    }
+  };
 
   return (
     <UnderPlayBar
@@ -18,9 +24,13 @@ function UnderPlayBarViewModel() {
       totalTime="00:30"
       onSoundInput={chanegeAudioVolume}
       isPlaying={isAudioPlaying}
-      onClickPlay={toggleAudio}
+      onClickPlay={handlePlayButtonClick}
     />
   );
 }
+
+UnderPlayBarViewModel.defaultProps = {
+  isRecording: false,
+};
 
 export default UnderPlayBarViewModel;
