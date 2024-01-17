c9bb3b3842bcbe61fdfdc8e14d2c1fc921dcc457
spiderq2697 <spiderq10@gmail.com>
2024-01-17 23:37:46 +0900

    [Issue-409] user.ts prettier 적용

--- client/src/mocks/handlers/user.ts
+++ client/src/mocks/handlers/user.ts
@@ -5,26 +5,25 @@ export const signUp = rest.get("http://localhost:5000/oauth2/login/:provider", (
     ctx.status(200),
     ctx.json({
       responseType: "SIGN_UP",
-        attributes: {
-          responseType: "SIGN_UP",
-          email: "collusic@gmail.com",
-          authId: "collusic",
-          profileImageUrl: "../../assets/signin/default_profile@3x.png",
-          snsType: req.params.provider,
-        }
-      }
-    ),
-  )
+      attributes: {
+        responseType: "SIGN_UP",
+        email: "collusic@gmail.com",
+        authId: "collusic",
+        profileImageUrl: "../../assets/signin/default_profile@3x.png",
+        snsType: req.params.provider,
+      },
+    }),
+  ),
 );
 
-export const checkNickName = rest.get("http://localhost:5000/members/:nickname", (req, res, ctx) => 
+export const checkNickName = rest.get("http://localhost:5000/members/:nickname", (req, res, ctx) =>
   res(
     ctx.status(200),
     ctx.json({
-      status : 200,
-      message : "사용 가능한 닉네임입니다."
+      status: 200,
+      message: "사용 가능한 닉네임입니다.",
     }),
-  )
+  ),
 );
 
 export const signIn = rest.post("http://localhost:5000/members", (req, res, ctx) =>
@@ -33,19 +32,19 @@ export const signIn = rest.post("http://localhost:5000/members", (req, res, ctx)
     ctx.json({
       responseType: "SIGN_IN",
       attributes: {
-        accessToken : "slkdfjaiefj.sefiajsef.sfiaejlf"
-      }
+        accessToken: "slkdfjaiefj.sefiajsef.sfiaejlf",
+      },
     }),
-    ctx.cookie('refeshToken', "dkfjaie.feiajfiose.fjsiae212d")
+    ctx.cookie("refeshToken", "dkfjaie.feiajfiose.fjsiae212d"),
   ),
 );
 
-export const reissue = rest.post("http://localhost:5000/reissue", (req, res, ctx) => 
+export const reissue = rest.post("http://localhost:5000/auth/reissue", (req, res, ctx) =>
   res(
     ctx.status(200),
     ctx.json({
-      status : 200,
-      message : "사용 가능한 닉네임입니다."
+      status: 200,
+      message: "사용 가능한 닉네임입니다.",
     }),
-  )
+  ),
 );
